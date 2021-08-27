62aa71db9185ecceac0963869af0c91548d20e12
GuYanSyue <88528224+GuYanSyue@users.noreply.github.com>
2021-08-28 02:05:36 +0800

    renew

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-let myName = '註冊成功' // string 字串
-let age = '我的年齡:800歲'
-let word = myName + age
-console.log(word)
-console.log(myName)
-
-let score = 200  // number 數字
-console.log(score)
-
-let isFake = true // boolean
-console.log(isFake)
-
-let bag = undefined  // 包包內沒東西
-console.log(bag)
-
-let bag2 = null
-console.log(bag2)
-
-
-
 const para = document.querySelector('span');
 
 para.addEventListener('click', updateName);

08567ac888594f328d136e67e98f25f82da39d87
GuYanSyue <88528224+GuYanSyue@users.noreply.github.com>
2021-09-12 22:16:07 +0800

    adjustment

--- foodStory-all.js
+++ foodStory-all.js
@@ -8,6 +8,24 @@ $(document).ready(function () {
         $('body').removeClass('open')
         //用 click 監聽 .mobile_close，當 click 觸發功能，會把 body 中的側邊選單關閉   
     });
+
+    var h1 = 400;
+    var h2 = 480;
+    var min_height = 100;
+    var ss = $(document).scrollTop();
+
+    $(window).scroll(function() {
+        var s = $(document).scrollTop();
+
+        if(s > h2){
+            if(s < ss){
+                $('.renew').fadeIn();
+            }else{
+                $('.renew').fadeOut();
+            }
+            ss = s;
+        }
+    });
 });
 
 
